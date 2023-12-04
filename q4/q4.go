d6c291a4452b9693f1c1dcc2987ea0210554c88c
IanMensitieri <141031523+IanMensitieri@users.noreply.github.com>
2023-12-04 00:07:02 -0300

    Update q4.go

--- q4/q4.go
+++ q4/q4.go
@@ -3,6 +3,41 @@ package q4
 // Você recebe uma lista de caminhos, onde `caminhos[i] = [cidadeAi, cidadeBi]` significa que existe um caminho direto que
 //vai de cidadeAi para cidadeBi. Retorne a cidade de destino, ou seja, a cidade sem nenhum caminho que saia dela.
 
+import (
+	"errors"
+	"fmt"
+)
+
 func Destino(caminhos [][2]string) (string, error) {
-	return "", nil
+	cidadesPartida := make(map[string]bool)
+
+	for _, caminho := range caminhos {
+		cidadesPartida[caminho[0]] = true
+	}
+
+	for _, caminho := range caminhos {
+		cidadeDestino := caminho[1]
+		if _, ok := cidadesPartida[cidadeDestino]; !ok {
+			return cidadeDestino, nil
+		}
+	}
+
+	return "", errors.New("nenhuma cidade de destino encontrada")
+}
+
+func main() {
+	caminhos := [][2]string{
+		{"A", "B"},
+		{"B", "C"},
+		{"C", "D"},
+		{"D", "E"},
+	}
+
+	destino, err := Destino(caminhos)
+
+	if err != nil {
+		fmt.Println("Erro:", err)
+	} else {
+		fmt.Println("Cidade de destino:", destino)
+	}
 }
